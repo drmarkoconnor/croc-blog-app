5c12cbf44f8f94580eb3db84134b05e715aec87a
Dr Mark O'Connor <dr.mark.oconnor@googlemail.com>
2025-08-13 23:43:56 +0100

    update

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,9 @@
 module.exports = function (eleventyConfig) {
 	// Passthrough static assets and hero images
-	eleventyConfig.addPassthroughCopy('src/assets')
-	eleventyConfig.addPassthroughCopy('src/_includes')
+	eleventyConfig.addPassthroughCopy({ 'src/assets': 'assets' })
+	// Expose quotes.json for client-side fetch
+	eleventyConfig.addPassthroughCopy({ 'src/_data/quotes.json': 'quotes.json' })
+	// Not required to copy includes, but harmless if left out
 	return {
 		dir: {
 			input: 'src/pages',
